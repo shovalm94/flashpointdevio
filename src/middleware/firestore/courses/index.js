5b4c9bc17c4a452cae3c69adb9a750b0f9d13148
shovalm <shovalm94@gmail.com>
2022-02-02 15:13:54 +0200

    add courses to their users

--- src/middleware/firestore/courses/index.js
+++ src/middleware/firestore/courses/index.js
@@ -6,6 +6,10 @@ import firebaseInstance from '../../firebase'
   function create(options){
     return firebaseInstance.db().collection(options.path).doc(options.id)
       .collection('students').doc(options.uid).set(options.user)
+      .then(()=>{
+          return firebaseInstance.db().collection('users').doc(options.uid).collection('courses')
+           .doc(options.id).set({[options.id]:options.id})
+      })
   }
 export default {
     read,
