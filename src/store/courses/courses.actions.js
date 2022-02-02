37b39059a6229e4c012b9d1e1934e5ccd5055dee
shovalm <shovalm94@gmail.com>
2022-02-02 21:37:14 +0200

    fix errors on the courses Page

--- src/store/courses/courses.actions.js
+++ src/store/courses/courses.actions.js
@@ -1,13 +1,15 @@
 import database from '../../middleware/firestore/courses'
 
-export default /* context */ {
-  getCourses: ({commit}) => {
-    let courses = []
-    database.read({path: 'courses'})
-      .then(result => result.docs.forEach(doc => {
-        courses.push({data: doc.data(), id: doc.id})
-      }))
-    commit('setCourses', courses)
+export default {
+  getCourses: ({commit,state}) => {
+    let courses = [];
+     database.read({path: 'courses'})
+      .then((result) =>{
+        result.docs.forEach(doc => {
+          courses.push({data: doc.data(), id: doc.id})
+      })
+        commit('setCourses',courses)
+      })
   },
   addStudents: ({commit, state}, item) => {
     database.create({path:'courses',id:item.id,uid:item.uid,user:item.user})
