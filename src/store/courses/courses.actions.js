ad8de0e47744f27975cb9757452a4d16aa3414b9
שרה כץ <katzsari1@gmail.com>
2022-02-10 11:17:07 +0000

    delete duplicates

--- src/store/courses/courses.actions.js
+++ src/store/courses/courses.actions.js
@@ -1,15 +1,6 @@
 import firebaseInstance from '../../middleware/firebase';
 
 export default /* context */ {
-  getCourses ({commit}){
-    let courses = []
-  firebaseInstance.db().collection('courses').get()
-    .then(result => result.docs.forEach(doc => {
-      courses.push(doc.data())
-    }))
-    commit('setCourses', courses)
-}
-
 
   getCourses({state, commit}) {
     let courses = []
